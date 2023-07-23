cc7b3d9ef5b6846489cd1fdbd8ed847d455806d2
Federico Burlon <federico.burlon@mia-platform.eu>
2023-07-23 12:16:14 +0200

    [Step 3] handle two customer mentions as start

--- src/__tests__/chatParser.test.ts
+++ src/__tests__/chatParser.test.ts
@@ -32,4 +32,34 @@ describe('Tests for chat parser', () => {
     expect(result).toStrictEqual(expected)
   });
 
+  it('should cover Step 3 (two customer mentions as start)', () => {
+    const input = '14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n' +
+      '14:27:00 Customer : Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.\n' +
+      '14:27:47 Agent : Vestibulum tempor diam eu leo molestie eleifend.\n' +
+      '14:28:28 Customer : Contrary to popular belief, Lorem Ipsum is not simply random text.'
+    const expected: ChatData[] = [{
+      date: '14:24:32',
+      mention: '14:24:32 Customer : ',
+      sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n',
+      type: OutputWriterType.CUSTOMER
+    }, {
+      date: '14:27:00',
+      mention: '14:27:00 Customer : ',
+      sentence: 'Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.\n',
+      type: OutputWriterType.CUSTOMER
+    }, {
+      date: '14:27:47',
+      mention: '14:27:47 Agent : ',
+      sentence: 'Vestibulum tempor diam eu leo molestie eleifend.\n',
+      type: OutputWriterType.AGENT
+    }, {
+      date: '14:28:28',
+      mention: '14:28:28 Customer : ',
+      sentence: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+      type: OutputWriterType.CUSTOMER
+    }]
+    const result = parseChat(input)
+    expect(result).toStrictEqual(expected)
+  });
+
 })
