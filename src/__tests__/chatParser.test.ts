64b18b59af0b25232e498d49af6d64508f861637
Federico Burlon <federico.burlon@mia-platform.eu>
2023-07-23 12:09:09 +0200

    [Step 2] handle two sentences

--- src/__tests__/chatParser.test.ts
+++ src/__tests__/chatParser.test.ts
@@ -13,4 +13,22 @@ describe('Tests for chat parser', () => {
     const result = parseChat(input)
     expect(result).toStrictEqual(expected)
   });
+
+  it('should cover Step 2 (two sentences)', () => {
+    const input = '14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n' +
+      '14:26:15 Agent : Aliquam non cursus erat, ut blandit lectus.'
+    const expected: ChatData[] = [{
+      date: '14:24:32',
+      mention: '14:24:32 Customer : ',
+      sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n',
+      type: WriterType.CUSTOMER
+    }, {
+      date: '14:26:15',
+      mention: '14:26:15 Agent : ',
+      sentence: 'Aliquam non cursus erat, ut blandit lectus.',
+      type: WriterType.AGENT
+    }]
+    const result = parseChat(input)
+    expect(result).toStrictEqual(expected)
+  });
 })
--- src/chatParser.ts
+++ src/chatParser.ts
@@ -1,10 +1,38 @@
 import {ChatData, WriterType} from "./models/Chat";
 
+const regex = /^(\d{2}:\d{2}:\d{2})\s+(?:Customer|Agent)\s+:\s+(.*)$/;
+
 export const parseChat = (chat: string): ChatData[] => {
-  return [{
-    date: '14:24:32',
-    mention: '14:24:32 Customer : ',
-    sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    type: WriterType.CUSTOMER
-  }]
+  const chatLines = chat.split('\n')
+  const chatDataArray: ChatData[] = []
+
+  let lineNumber = 1
+  let isLastLine = false
+  chatLines.forEach( line => {
+
+      isLastLine = lineNumber == chatLines.length
+
+      const match = line.match(regex);
+
+      if (match) {
+        const sentence = match[2]
+        const mention = line.replace(sentence, '')
+        const date = match[1]
+        const type = mention.includes(WriterType.CUSTOMER) ? WriterType.CUSTOMER : WriterType.AGENT;
+        const chatData: ChatData = {
+          mention,
+          date,
+          type,
+          sentence: isLastLine ? sentence : `${sentence}\n`
+        }
+
+        console.log(chatData, 'chatData found!')
+        chatDataArray.push(chatData);
+
+      } else {
+        console.error('We should never end up here hopefully')
+      }
+    lineNumber++
+    })
+  return chatDataArray
 }
